b61a28f5e9f5b0f8a6caae9fcc964748f2aa56d6
fremasadi <fremasadii@gmail.com>
2025-03-13 13:24:32 +0700

    feat:order completed where userID

--- app/Filament/Resources/AttendanceResource.php
+++ app/Filament/Resources/AttendanceResource.php
@@ -15,98 +15,98 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Tables\Columns\TextColumn;
 
 
-// class AttendanceResource extends Resource
-// {
-//     protected static ?string $model = Attendance::class;
+class AttendanceResource extends Resource
+{
+    protected static ?string $model = Attendance::class;
 
-//     protected static ?string $navigationIcon = 'heroicon-o-calendar-days';
+    protected static ?string $navigationIcon = 'heroicon-o-calendar-days';
 
-//     protected static ?string $navigationLabel = 'Absensi Karyawan';
+    protected static ?string $navigationLabel = 'Absensi Karyawan';
 
-//     public static function getModelLabel(): string
-//     {
-//         return 'Absensi';
-//     }
+    public static function getModelLabel(): string
+    {
+        return 'Absensi';
+    }
 
-//     public static function getPluralModelLabel(): string
-//     {
-//         return 'Daftar Absensi';
-//     }
+    public static function getPluralModelLabel(): string
+    {
+        return 'Daftar Absensi';
+    }
 
 
-//     public static function getNavigationGroup(): ?string
-//     {
-//         return 'Manajemen Absensi';
-//     }
+    public static function getNavigationGroup(): ?string
+    {
+        return 'Manajemen Absensi';
+    }
 
-//     public static function form(Form $form): Form
-//     {
-//         return $form
-//             ->schema([
-//                 Forms\Components\TextInput::make('user_id')
-//                     ->required()
-//                     ->numeric(),
-//                 Forms\Components\DatePicker::make('date')
-//                     ->required(),
-//                 Forms\Components\TextInput::make('check_in'),
-//                 Forms\Components\TextInput::make('status')
-//                     ->required(),
-//                 Forms\Components\TextInput::make('late_minutes')
-//                     ->numeric(),
-//             ]);
-//     }
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('user_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\DatePicker::make('date')
+                    ->required(),
+                Forms\Components\TextInput::make('check_in'),
+                Forms\Components\TextInput::make('status')
+                    ->required(),
+                Forms\Components\TextInput::make('late_minutes')
+                    ->numeric(),
+            ]);
+    }
 
-//     public static function table(Table $table): Table
-//     {
-//         return $table
-//             ->columns([
-//                 TextColumn::make('user.name')
-//                     ->label('Nama Karyawan')
-//                     ->searchable()
-//                     ->sortable(),
-//                 Tables\Columns\TextColumn::make('date')
-//                     ->date()
-//                     ->sortable(),
-//                 Tables\Columns\TextColumn::make('check_in'),
-//                 Tables\Columns\TextColumn::make('status'),
-//                 Tables\Columns\TextColumn::make('late_minutes')
-//                     ->numeric()
-//                     ->sortable(),
-//                 Tables\Columns\TextColumn::make('created_at')
-//                     ->dateTime()
-//                     ->sortable()
-//                     ->toggleable(isToggledHiddenByDefault: true),
-//                 Tables\Columns\TextColumn::make('updated_at')
-//                     ->dateTime()
-//                     ->sortable()
-//                     ->toggleable(isToggledHiddenByDefault: true),
-//             ])
-//             ->filters([
-//                 //
-//             ])
-//             ->actions([
-//                 Tables\Actions\EditAction::make(),
-//             ])
-//             ->bulkActions([
-//                 Tables\Actions\BulkActionGroup::make([
-//                     Tables\Actions\DeleteBulkAction::make(),
-//                 ]),
-//             ]);
-//     }
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('user.name')
+                    ->label('Nama Karyawan')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('date')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('check_in'),
+                Tables\Columns\TextColumn::make('status'),
+                Tables\Columns\TextColumn::make('late_minutes')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
 
-//     public static function getRelations(): array
-//     {
-//         return [
-//             //
-//         ];
-//     }
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
 
-//     public static function getPages(): array
-//     {
-//         return [
-//             'index' => Pages\ListAttendances::route('/'),
-//             'create' => Pages\CreateAttendance::route('/create'),
-//             'edit' => Pages\EditAttendance::route('/{record}/edit'),
-//         ];
-//     }
-// }
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAttendances::route('/'),
+            'create' => Pages\CreateAttendance::route('/create'),
+            'edit' => Pages\EditAttendance::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Filament/Resources/SalarySettingResource.php
+++ app/Filament/Resources/SalarySettingResource.php
@@ -15,133 +15,133 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Select;
 
-// class SalarySettingResource extends Resource
-// {
-//     protected static ?string $model = SalarySetting::class;
+class SalarySettingResource extends Resource
+{
+    protected static ?string $model = SalarySetting::class;
 
-//     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-//     protected static ?string $navigationLabel = 'Pengaturan Gaji';
+    protected static ?string $navigationLabel = 'Pengaturan Gaji';
 
-//     public static function getModelLabel(): string
-//     {
-//         return 'Pengaturan Gaji Karyawan';
-//     }
+    public static function getModelLabel(): string
+    {
+        return 'Pengaturan Gaji Karyawan';
+    }
 
-//     public static function getPluralModelLabel(): string
-//     {
-//         return 'Pengaturan Daftar Gaji';
-//     }
-
-
-//     public static function getNavigationGroup(): ?string
-//     {
-//         return 'Manajemen Gaji';
-//     }
+    public static function getPluralModelLabel(): string
+    {
+        return 'Pengaturan Daftar Gaji';
+    }
 
 
-//     public static function form(Form $form): Form
-//     {
-//         return $form
-//             ->schema([
-//                 TextInput::make('name')
-//                 ->label('Nama Setting Gaji')
-//                 ->required()
-//                 ->maxLength(255),
+    public static function getNavigationGroup(): ?string
+    {
+        return 'Manajemen Gaji';
+    }
 
-//             TextInput::make('salary')
-//                 ->label('Gaji Pokok')
-//                 ->numeric()
-//                 ->required()
-//                 ->prefix('Rp')
-//                 ->rules(['min:0']),
 
-//             Select::make('periode')
-//                 ->label('Periode Penggajian')
-//                 ->options([
-//                     'daily' => 'Harian',
-//                     'weekly' => 'Mingguan',
-//                     'monthly' => 'Bulanan',
-//                 ])
-//                 ->required(),
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                ->label('Nama Setting Gaji')
+                ->required()
+                ->maxLength(255),
 
-//             TextInput::make('reduction_if_absent')
-//                 ->label('Potongan Jika Tidak Hadir')
-//                 ->numeric()
-//                 ->required()
-//                 ->prefix('Rp')
-//                 ->rules(['min:0']),
+            TextInput::make('salary')
+                ->label('Gaji Pokok')
+                ->numeric()
+                ->required()
+                ->prefix('Rp')
+                ->rules(['min:0']),
 
-//             TextInput::make('permit_reduction')
-//                 ->label('Potongan Jika Izin')
-//                 ->numeric()
-//                 ->required()
-//                 ->prefix('Rp')
-//                 ->rules(['min:0']),
-//                 Forms\Components\TextInput::make('deduction_per_minute')
-//                 ->label('Potongan Telat Permenit')
-//                 ->numeric()
-//                 ->required()
-//                 ->prefix('Rp')
-//                 ->rules(['min:0']),
+            Select::make('periode')
+                ->label('Periode Penggajian')
+                ->options([
+                    'daily' => 'Harian',
+                    'weekly' => 'Mingguan',
+                    'monthly' => 'Bulanan',
+                ])
+                ->required(),
 
-//             ]);
-//     }
+            TextInput::make('reduction_if_absent')
+                ->label('Potongan Jika Tidak Hadir')
+                ->numeric()
+                ->required()
+                ->prefix('Rp')
+                ->rules(['min:0']),
 
-//     public static function table(Table $table): Table
-//     {
-//         return $table
-//             ->columns([
-//                 Tables\Columns\TextColumn::make('name')
-//                     ->searchable(),
-//                 Tables\Columns\TextColumn::make('salary')
-//                     ->numeric()
-//                     ->sortable(),
-//                 Tables\Columns\TextColumn::make('periode'),
-//                 Tables\Columns\TextColumn::make('reduction_if_absent')
-//                     ->numeric()
-//                     ->sortable(),
-//                 Tables\Columns\TextColumn::make('permit_reduction')
-//                     ->numeric()
-//                     ->sortable(),
-//                 Tables\Columns\TextColumn::make('deduction_per_minute')
-//                     ->numeric()
-//                     ->sortable(),
-//                 Tables\Columns\TextColumn::make('created_at')
-//                     ->dateTime()
-//                     ->sortable()
-//                     ->toggleable(isToggledHiddenByDefault: true),
-//                 Tables\Columns\TextColumn::make('updated_at')
-//                     ->dateTime()
-//                     ->sortable()
-//                     ->toggleable(isToggledHiddenByDefault: true),
-//             ])
-//             ->filters([
-//                 //
-//             ])
-//             ->actions([
-//                 Tables\Actions\EditAction::make(),
-//             ])
-//             ->bulkActions([
-//                 Tables\Actions\BulkActionGroup::make([
-//                     Tables\Actions\DeleteBulkAction::make(),
-//                 ]),
-//             ]);
-//     }
+            TextInput::make('permit_reduction')
+                ->label('Potongan Jika Izin')
+                ->numeric()
+                ->required()
+                ->prefix('Rp')
+                ->rules(['min:0']),
+                Forms\Components\TextInput::make('deduction_per_minute')
+                ->label('Potongan Telat Permenit')
+                ->numeric()
+                ->required()
+                ->prefix('Rp')
+                ->rules(['min:0']),
 
-//     public static function getRelations(): array
-//     {
-//         return [
-//             //
-//         ];
-//     }
+            ]);
+    }
 
-//     public static function getPages(): array
-//     {
-//         return [
-//             'index' => Pages\ListSalarySettings::route('/'),
-//             'create' => Pages\CreateSalarySetting::route('/create'),
-//             'edit' => Pages\EditSalarySetting::route('/{record}/edit'),
-//         ];
-//     }
-// }
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('salary')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('periode'),
+                Tables\Columns\TextColumn::make('reduction_if_absent')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('permit_reduction')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('deduction_per_minute')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSalarySettings::route('/'),
+            'create' => Pages\CreateSalarySetting::route('/create'),
+            'edit' => Pages\EditSalarySetting::route('/{record}/edit'),
+        ];
+    }
+}
