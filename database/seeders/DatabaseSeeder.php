4faf12c1a7ebe032ca8d799041501bc443494c29
fremasadi <fremasadii@gmail.com>
2025-04-21 00:13:16 +0700

    feat:reset data table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Seeder user admin
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password123'), // Ganti password sesuai kebutuhan
+            'address' => 'Jl.No. 123',
+            'phone' => '081234567890',
+            'image' => null, // Atau isi dengan path image jika ada
+            'role' => 'admin',
         ]);
     }
 }
