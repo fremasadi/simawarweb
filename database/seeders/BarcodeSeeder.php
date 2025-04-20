903754819a4a615cbd3c127ad793ff2be43290a6
fremasadi <fremasadii@gmail.com>
2025-04-21 00:18:06 +0700

    feat:setting gaji

---
+++ database/seeders/BarcodeSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BarcodeSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('barcodes')->insert([
+            'kode' => 'absenmasuk',
+            'created_at' => '2025-02-25 16:21:09',
+            'updated_at' => '2025-02-25 16:21:09',
+        ]);
+    }
+}
