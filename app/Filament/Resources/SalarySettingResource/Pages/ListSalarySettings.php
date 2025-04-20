0eafca8ee45e81abcc8a5fef1b573f84c5d3cd2d
fremasadi <fremasadii@gmail.com>
2025-04-21 00:14:29 +0700

    feat:setting gaji

--- app/Filament/Resources/SalarySettingResource/Pages/ListSalarySettings.php
+++ app/Filament/Resources/SalarySettingResource/Pages/ListSalarySettings.php
@@ -13,7 +13,7 @@ class ListSalarySettings extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            // Actions\CreateAction::make(),
+            Actions\CreateAction::make(),
         ];
     }
 }
