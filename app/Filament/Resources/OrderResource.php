0931dcf9d8a00858e32b248ce27ad5a853c0f4c7
fremasadi <fremasadii@gmail.com>
2025-04-28 22:25:09 +0700

    feat:api wa create order

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -136,29 +136,30 @@ class OrderResource extends Resource
                     ];
                 })
                 ->columnSpanFull()
-                ->afterCreate(function ($record, $data) {
-                    // Setelah data berhasil disimpan ke database
-        
-                    $phone = $record->phone; // Ambil phone dari record
-                    $message = "Terima kasih telah memesan, pesanan Anda akan segera kami proses.";
-        
-                    try {
-                        $response = Http::withHeaders([
-                            'Authorization' => 'YuHu739HMs1gtWXzD1X7',
-                        ])->post('https://api.fonnte.com/send', [
-                            'target' => $phone,
-                            'message' => $message,
-                            'countryCode' => '62',
-                        ]);
-        
-                        if ($response->failed()) {
-                            logger()->error('Gagal mengirim WhatsApp: ' . $response->body());
-                        }
-                    } catch (\Exception $e) {
-                        logger()->error('Error mengirim WhatsApp: ' . $e->getMessage());
+                
+            ])
+            ->afterCreate(function ($record, $data) {
+                // Setelah data berhasil disimpan ke database
+    
+                $phone = $record->phone; // Ambil phone dari record
+                $message = "Terima kasih telah memesan, pesanan Anda akan segera kami proses.";
+    
+                try {
+                    $response = Http::withHeaders([
+                        'Authorization' => 'YuHu739HMs1gtWXzD1X7',
+                    ])->post('https://api.fonnte.com/send', [
+                        'target' => $phone,
+                        'message' => $message,
+                        'countryCode' => '62',
+                    ]);
+    
+                    if ($response->failed()) {
+                        logger()->error('Gagal mengirim WhatsApp: ' . $response->body());
                     }
-                })
-            ]);
+                } catch (\Exception $e) {
+                    logger()->error('Error mengirim WhatsApp: ' . $e->getMessage());
+                }
+            });
     }
 
     public static function table(Table $table): Table
