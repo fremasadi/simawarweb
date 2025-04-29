1cefec727b41849079ff2baeeeadb0ffeb95eff6
fremasadi <fremasadii@gmail.com>
2025-04-30 01:21:32 +0700

    absensi

--- app/Filament/Resources/AttendanceResource.php
+++ app/Filament/Resources/AttendanceResource.php
@@ -88,7 +88,7 @@ class AttendanceResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                // Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
