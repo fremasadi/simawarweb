d37b3d19acec1186918da0d141eae6fdfb1bd950
fremasadi <fremasadii@gmail.com>
2025-04-30 23:54:04 +0700

    feat gaji api

--- app/Http/Controllers/SalaryHistoryController.php
+++ app/Http/Controllers/SalaryHistoryController.php
@@ -2,7 +2,7 @@
 
 // app/Http/Controllers/Api/SalaryHistoryController.php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
