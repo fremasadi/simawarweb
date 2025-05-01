734b9dbf1e61aac6538bccaf3cdc5c9d75f4a1ee
fremasadi <fremasadii@gmail.com>
2025-05-02 03:43:05 +0700

    feat edit

--- app/Filament/Resources/SalarySettingResource/Pages/EditSalarySetting.php
+++ app/Filament/Resources/SalarySettingResource/Pages/EditSalarySetting.php
@@ -7,6 +7,8 @@ use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Filament\Notifications\Notification;
 use Illuminate\Support\Facades\Artisan;
+use App\Models\Salary;
+
 class EditSalarySetting extends EditRecord
 {
     protected static string $resource = SalarySettingResource::class;
@@ -14,18 +16,19 @@ class EditSalarySetting extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            // Actions\DeleteAction::make(),
         ];
     }
 
     protected function afterSave(): void
     {
-        // Jalankan perintah Artisan
-        Artisan::call('salary:calculate-deductions');
-
+        // Update semua data gaji yang terkait dengan pengaturan gaji ini
+        Salary::where('salary_setting_id', $this->record->id)
+            ->update(['total_salary' => $this->record->salary]);
+            
         // Tampilkan notifikasi sukses
         Notification::make()
-            ->title('Perhitungan potongan berhasil dijalankan')
+            ->title('Pengaturan gaji berhasil disimpan')
+            ->body('Total gaji karyawan berhasil diperbarui')
             ->success()
             ->send();
     }
