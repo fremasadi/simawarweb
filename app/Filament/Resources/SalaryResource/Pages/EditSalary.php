0a3b9120d32d49dec420e79c0fe77e466a66d671
fremasadi <fremasadii@gmail.com>
2025-05-17 20:56:40 +0700

    potongan gaji

--- app/Filament/Resources/SalaryResource/Pages/EditSalary.php
+++ app/Filament/Resources/SalaryResource/Pages/EditSalary.php
@@ -13,7 +13,7 @@ class EditSalary extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            // Actions\DeleteAction::make(),
         ];
     }
 }
